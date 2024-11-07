70392aa0dcd6a94cd4f290e195b67f89474ca45e
Douglas Baptista de Godoy <douglasbgodoy@gmail.com>
2024-11-06 21:30:05 -0300

    Create 1-Exemplo_pratico.py

---
+++ CODE/7-POO/1-Exemplo_pratico.py
@@ -0,0 +1,11 @@
+""" 
+ * Nome: Exemplos de POO
+ * Descrição: Ao final da aula, os alunos serão capazes de  
+ *            trabalhar com Programação Orientada a Objetos.
+ * Autor: Douglas Baptista de Godoy
+ * Data de Criação: 2024-11-06
+ * Versão: 1.0
+ * 
+ * Dependências:
+ * - Python 3.12.6
+"""
